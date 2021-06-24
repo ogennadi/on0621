e0c4058161f3607195539ed62cb00e75ca8db75b
Oge Nnadi <oge@nnadi.org>
2021-06-24 03:51:42 -0500

    Give exceptions user-friendly messages

--- src/main/java/PercentOutOfBoundsException.java
+++ src/main/java/PercentOutOfBoundsException.java
@@ -1,2 +1,6 @@
 public class PercentOutOfBoundsException extends RuntimeException{
+    @Override
+    public String toString() {
+        return "PercentOutOfBoundsException: The percentage entered is less than 0 or greater than 100";
+    }
 }
--- src/main/java/RentalDayCountOutOfBoundsException.java
+++ src/main/java/RentalDayCountOutOfBoundsException.java
@@ -1,2 +1,6 @@
 public class RentalDayCountOutOfBoundsException extends RuntimeException{
+    @Override
+    public String toString() {
+        return "RentalDayCountOutOfBoundsException: The rental day count is 0 or less.";
+    }
 }
